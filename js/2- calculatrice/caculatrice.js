df93f06ee80b6c8eb1fc23d55fc18614d381e0e4
3th4nol <3th4nol.pro@gmail.com>
2022-12-06 17:06:19 +0100

    v4

--- js/2- calculatrice/caculatrice.js
+++ js/2- calculatrice/caculatrice.js
@@ -1,8 +1,23 @@
-function afficher(valeur){
-    let affichage = document.getElementById("affichage")
+let un = document.getElementById("un")
+let deux = document.getElementById("deux")
+let trois = document.getElementById("trois")
+let clear = document.getElementById("clear")
+let égal = document.getElementById("égal")
 
-    affichage.innerHTML = valeur
-}
-function calculer(){
-    
+let quatre = document.getElementById("quatre")
+let cinq = document.getElementById("cinq")
+let six = document.getElementById("six")
+let fois = document.getElementById("fois")
+let diviser = document.getElementById("diviser")
+
+let sept = document.getElementById("sept")
+let huit = document.getElementById("huit")
+let neuf = document.getElementById("neuf")
+let plus = document.getElementById("plus")
+let moins = document.getElementById("moins")
+
+let zero = document.getElementById("zero")
+
+function afficher(){
+
 }
--- js/3- to do list/index.js
+++ js/3- to do list/index.js
@@ -1,9 +1,6 @@
 let toDoItems = []
 
-
-let id = 0; // là faut que ce soit pas 0 mais la valeur du localStorage pour que les ids continuent
-
-// là faut lire ce que t'as dans le localStorage et l'afficher
+let id = 0;
 
 function Add(){
 
@@ -24,19 +21,22 @@ function Add(){
                                 <div class="flexend"><div class="bordure"><img src="crayon.png" class="crayon" alt="crayon"></div><button onclick=Delete(${id}) class="button">x</button></div></div>`}       
     
     toDoItems.push(MessageData)
-    // Pour la prochaine fois faudra stocker le tableau entier dans un seul truc pour pouvoir mettre d'autres choses qui ont rien a voir dans le localStorage
-    localStorage.setItem(MessageData.id, MessageData.text)
+    
+    localStorage.setItem('user', JSON.stringify(toDoItems))
 
     document.getElementById("list").innerHTML = toDoItems.reduce((total, toDoItem) => total + toDoItem.html, "")
+}
 
-
-
-    }
 
 function Delete(id){
     toDoItems.splice(toDoItems.findIndex((messagedata)=> id === messagedata.id), 1)
     document.getElementById("list").innerHTML = toDoItems.reduce((total, toDoItem) => total + toDoItem.html, "")
 }
+let items = localStorage.getItem('user')
 
+if (items !== null){
 
+toDoItems = JSON.parse(items)
 
+document.getElementById("list").innerHTML = toDoItems.reduce((total, toDoItem) => total + toDoItem.html, "")
+}
