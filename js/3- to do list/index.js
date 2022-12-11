5591166d26eb04ce79cefb60cd5c41710d4c9376
ethan.aubry5 <ethan.aubry5@monlycee.net>
2022-12-11 10:44:53 +0100

    v5

--- js/3- to do list/index.js
+++ js/3- to do list/index.js
@@ -2,41 +2,104 @@ let toDoItems = []
 
 let id = 0;
 
+let MessageData = {}
+
+let list = document.getElementById("list")
+
 function Add(){
 
     if(toDoItems.length >= 5){
         return
     }
-
     let text = document.getElementById("text").value 
+
     if(text === "") return
+
+    MessageData = { 
+        id: id,
+        text : text,
+        html : `<div class="flex" onclick=click() id="${id}"><div class="contenant"><h1 class="check">✓</h1><li class="li" id=${id} >${text}</li>
+                <div class="flexend"><div class="bordure"><img src="crayon.png" class="crayon" alt="crayon"></div><button onclick=Delete(${id}) class="button">x</button></div></div>`}       
     
-    id ++
+
+                
+    
     document.getElementById("text").value = ""
 
-    let MessageData = { id: id,
-                        text : text,
-                        html : `<div class="flex">
-                                <div class="contenant"><h1 class="check">✓</h1><li class="li">${text}</li>
-                                <div class="flexend"><div class="bordure"><img src="crayon.png" class="crayon" alt="crayon"></div><button onclick=Delete(${id}) class="button">x</button></div></div>`}       
-    
     toDoItems.push(MessageData)
     
     localStorage.setItem('user', JSON.stringify(toDoItems))
 
-    document.getElementById("list").innerHTML = toDoItems.reduce((total, toDoItem) => total + toDoItem.html, "")
+    list.insertAdjacentHTML('afterbegin', MessageData.html) 
+    
+    toDoItems.reduce((total, toDoItem) => total + toDoItem.html, "")
+        
+    id ++
+
+    for(let flexOne of flex){
+        flexOne.addEventListener('mouseenter', () =>{
+            flexOne.style.backgroundColor = "rgb(220,220,220)"
+        })
+        flexOne.addEventListener('mouseleave', () =>{
+            flexOne.style.backgroundColor = "white"
+    
+        }) 
+        for(let checkOne of check){
+            flexOne.addEventListener('mouseenter', () =>{
+                checkOne.style.display = "block"
+            })
+            flexOne.addEventListener('mouseleave', () =>{
+                checkOne.style.display = "none"
+        
+            }) 
+            }
+        }
 }
+function Delete(id) {
+    toDoItems.splice(toDoItems[id],1)
+    let child = document.getElementById(id)
+    list.removeChild(child)
 
-
-function Delete(id){
-    toDoItems.splice(toDoItems.findIndex((messagedata)=> id === messagedata.id), 1)
-    document.getElementById("list").innerHTML = toDoItems.reduce((total, toDoItem) => total + toDoItem.html, "")
 }
 let items = localStorage.getItem('user')
 
+
 if (items !== null){
-
-toDoItems = JSON.parse(items)
-
-document.getElementById("list").innerHTML = toDoItems.reduce((total, toDoItem) => total + toDoItem.html, "")
+    for (id = 0; id <= toDoItems.length; id ++){
+        toDoItems = JSON.parse(items)
+        let parsed = toDoItems[id]?.html
+        if (parsed !== undefined){
+            list.insertAdjacentHTML('afterbegin', parsed)
+            toDoItems.reduce((total, toDoItem) => total + toDoItem.html, "")
+        }
+    }
 }
+// id = items.length
+let check = document.getElementsByClassName("check")
+let lala = document.getElementsByClassName("li")
+
+
+
+    let flex = document.getElementsByClassName("flex") 
+
+    for(let flexOne of flex){
+        flexOne.addEventListener('mouseenter', () =>{
+            flexOne.style.backgroundColor = "rgb(220,220,220)"
+        })
+        flexOne.addEventListener('mouseleave', () =>{
+            flexOne.style.backgroundColor = "white"
+
+        }) 
+        
+            for(let checkOne of check){
+                flexOne.addEventListener('mouseenter', () =>{
+                    checkOne.style.display = "block"
+                })
+                flexOne.addEventListener('mouseleave', () =>{
+                    checkOne.style.display = "none"
+            
+                }) 
+                
+            } 
+        }
+       
